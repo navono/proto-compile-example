3e17bb29e3d5c236112201ac2f80c94ceccc52f5
navono <navono007@gmail.com>
2019-06-13 08:49:08 +0800

    feat: add batch compile

--- build.cmd
+++ build.cmd
@@ -1,17 +1,41 @@
+@ECHO OFF
+REM 声明采用UTF-8编码
+chcp 65001
+echo "请先安装 Golang 和 protoc，然后安装 protoc 插件"
 
-echo off
+set protoDir=./api
+for /r "%protoDir%" %%f in (*.proto) do (
+  call :concatFiles "%%f"
+)
 
-REM echo %cd%
-REM docker run --rm -v %cd%:%cd% -w %cd% znly/protoc --go_out=plugins=grpc::. -I/bazel-proto/api ./api/thing.proto
-REM docker run --rm -v "E://sourcecode//bazel-proto//":"E://sourcecode//bazel-proto//" -w "E://sourcecode//bazel-proto" znly/protoc --go_out=plugins=grpc::. -I/bazel-proto/api ./api/thing.proto
+goto :END
 
-REM docker run --rm -v %cd%:/data -w /data alpine ls
+:concatFiles
+REM echo :%1
+set file=%~n1%~x1
+set files=%files% %file%
+GOTO :eof
 
-REM list:=$(shell find ./vxsip -iname "*.proto")
-REM proto_files = $(addprefix repo/, $(list))
+:END
+REM echo %files%
+set PROTOC_GEN_TS_PATH=%cd%\\node_modules\\.bin\\protoc-gen-ts.cmd
 
-docker run --rm -v %cd%:/data -w /data znly/protoc -Iapi -Ithird_party ^
-  --go_out=plugins=grpc:out ^
-  --grpc-gateway_out=logtostderr=true:out ^
-  --lint_out=. ^
-  api/greeter.proto
+REM Directory to write generated code to
+set OUT_DIR="./generated"
+set JS_DIR=%OUT_DIR%/js
+set GO_DIR=%OUT_DIR%/go
+set SWAGGER_DIR=%OUT_DIR%/swagger
+
+mkdir %JS_DIR%
+mkdir %GO_DIR%
+mkdir %SWAGGER_DIR%
+
+protoc -I%protoDir% -Ithird_party ^
+      --plugin="protoc-gen-ts=%PROTOC_GEN_TS_PATH%" ^
+      --js_out=import_style=commonjs,binary:%JS_DIR% ^
+			--ts_out=service=true:%JS_DIR% ^
+      --go_out=plugins=grpc:%GO_DIR% ^
+      --grpc-gateway_out=logtostderr=true:%GO_DIR% ^
+      --swagger_out=logtostderr=true:%SWAGGER_DIR% ^
+      --lint_out=. ^
+      %files%
