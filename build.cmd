8f90e4970dcbc223fb92c787248f48b4c81b31f1
navono <navono007@gmail.com>
2019-06-13 08:52:44 +0800

    fix build batch

--- build.cmd
+++ build.cmd
@@ -3,21 +3,6 @@ REM 声明采用UTF-8编码
 chcp 65001
 echo "请先安装 Golang 和 protoc，然后安装 protoc 插件"
 
-set protoDir=./api
-for /r "%protoDir%" %%f in (*.proto) do (
-  call :concatFiles "%%f"
-)
-
-goto :END
-
-:concatFiles
-REM echo :%1
-set file=%~n1%~x1
-set files=%files% %file%
-GOTO :eof
-
-:END
-REM echo %files%
 set PROTOC_GEN_TS_PATH=%cd%\\node_modules\\.bin\\protoc-gen-ts.cmd
 
 REM Directory to write generated code to
@@ -30,7 +15,26 @@ mkdir %JS_DIR%
 mkdir %GO_DIR%
 mkdir %SWAGGER_DIR%
 
-protoc -I%protoDir% -Ithird_party ^
+
+set protoDir=./api
+for /r "%protoDir%" %%f in (*.proto) do (
+  call :concatFiles "%%f"
+)
+
+goto :eof
+
+:concatFiles
+REM echo %1
+
+set filepath=%1
+REM 删除 vxsip\ 之前的所有字符
+set tempfile=%filepath:*bazel-proto\=%
+REM 将 \ 替换成 /
+set file=%tempfile:\=/%
+REM 删除最后一个 "
+set file=%file:~0, -1%
+
+protoc -I. -Ithird_party ^
       --plugin="protoc-gen-ts=%PROTOC_GEN_TS_PATH%" ^
       --js_out=import_style=commonjs,binary:%JS_DIR% ^
 			--ts_out=service=true:%JS_DIR% ^
@@ -38,4 +42,5 @@ protoc -I%protoDir% -Ithird_party ^
       --grpc-gateway_out=logtostderr=true:%GO_DIR% ^
       --swagger_out=logtostderr=true:%SWAGGER_DIR% ^
       --lint_out=. ^
-      %files%
+      %file%
+GOTO :eof
